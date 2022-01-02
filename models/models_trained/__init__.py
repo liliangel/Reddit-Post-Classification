5bd655bc3316e717b5a39a8de0fb9e4b56b83328
liliangel <liliangel@users.noreply.github.com>
2022-01-02 05:46:57 +0200

    Add files via upload
    model pretrained

---
+++ models/models_trained/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan  2 00:44:52 2022
+
+@author: lilia
+"""
+
---
+++ models/models_trained/temp.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 31 16:55:25 2021
+
+@author: lilia
+"""
+
+import os
+
+print(os.getcwd())
